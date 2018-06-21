412df9deb811cadbab2eb2589df1abc41ba1f5e1
Junhson <jjeanba2@binghamton.edu>
2018-06-20 23:21:12 -0400

    Added a bit more commentary (comments)

--- src/main.c
+++ src/main.c
@@ -1,18 +1,20 @@
-
+/* Ain't no party like a Holy Ghost party, because a Holy Ghost party don't stop! */
 
 void shellebrate_loop(void) {
 
-    char * line;
-    char ** args;
+    // A few declarations to start the party!
+    char * line; // store the entered line here
+    char ** args; // store the list of args here
     int status;
 
     do {
 
-        printf(">")
-        line = shell_read();
-        args = shell_split();
-        status = shell_execute();
+        printf(">"); // print a little arrow so the user knows where to enter commands
+        line = shell_read(); // read the entered line
+        args = shell_split(); // split the input into arguments
+        status = shell_execute(); // do that thing!
 
+        // Free up a little bit of space.
         free(line);
         free(args);
 
@@ -25,6 +27,7 @@ int main(int argc, char** argv) {
     // Call the loop function to get this party started!
     shellebrate_loop();
 
+    // Leave that party like a boss
     return EXIT_SUCCESS
 
 }
