e708150dc63effb501cf35f0ba895910907e1ed4
Junhson <jjeanba2@binghamton.edu>
2018-06-29 19:05:55 -0400

    wrote all code, but cmds get no response must fix

--- src/main.c
+++ src/main.c
@@ -1,10 +1,123 @@
 /* Ain't no party like a Holy Ghost party, because a Holy Ghost party don't stop! */
+
+// Some preprocessor definitions and includes
 #define SHELL_RL_BUFSIZE 1024
 #define SHELL_TOK_BUFSIZE 64
 #define SHELL_TOK_DELIM " \t\r\n\a"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/* Declarations of built-in shell command functions */
+int shell_cd(char ** args);
+int shell_help(char ** args);
+int shell_exit(char ** args);
+
+/* Declarations of the rest of the functions */
+int shell_num_builtins();
+int shell_execute(char ** args);
+char * shell_read();
+char ** shell_split(char * line);
+int shell_launch(char ** args);
+void shellebrate_loop(void);
+
+/* ========= STUFF FOR BUILT IN COMMANDS ========= */
+
+/* List of built-in commands */
+char * builtins_string[] = {
+    "shell_cd",
+    "shell_help",
+    "shell_exit"
+};
+
+/* List of build-in command function pointers */
+int (*builtin_func[]) (char **) = {
+    &shell_cd,
+    &shell_help,
+    &shell_exit
+};
+
+/* Get the number of buil in commands */
+int shell_num_builtins() {
+    return sizeof(builtins_string) / sizeof(char *);
+}
+
+/* Actual Definitions of built-in functions */
+
+// cd function
+int shell_cd(char ** args) {
+
+    if (args[1] == NULL) {
+        fprintf(stderr, "shellebrate: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("lsh");
+        }
+    }
+
+    return 1;
+
+}
+
+// help function
+int shell_help(char ** args) {
+
+    printf("Welcom to Shellebrate! A shell by Junhson Jean-Baptiste.\n");
+    printf("Type in a program name and it's necessary arguments.\n");
+    printf("Here are the built in commands: \n");
+    for (int i = 0; i < shell_num_builtins(); i++) {
+
+        printf("  %s\n", builtins_string[i]);
+
+    }
+    
+
+    return 1;
+
+}
+
+// exit function
+int shell_exit(char ** args) {
+    return 0;
+}
+
+/* ========= END OF STUFF FOR BUILT IN COMMANDS ========= */
+
+/* ========= THE GOOD STUFF ========= */
+
+/* Our main function */
+int main(int argc, char** argv) {
+
+    // Call the loop function to get this party started!
+    shellebrate_loop();
+
+    // Leave that party like a boss
+    return EXIT_SUCCESS;
+
+}
+
+int shell_execute(char ** args) {
+
+    // Handle empty commands
+    if (args[1] == NULL) {
+        return 1;
+    }
+
+    // Check if the command is one of the builtin in ones and handle it if so
+    for (int i = 0; i < shell_num_builtins(); i++) {
+
+        if ( strcmp(args[0], builtins_string[i]) == 0 ) {
+            return (* builtin_func[i]) (args);
+        }
+
+    }
+
+    // Otherwise return the program command (not built in) that was called
+    return shell_launch(args);
+
+}
 
 char * shell_read() {
 
@@ -93,7 +206,7 @@ char ** shell_split(char * line) {
 
 }
 
-int lsh_launch(char ** args) {
+int shell_launch(char ** args) {
 
     pid_t pid, wpid;
     int status;
@@ -148,12 +261,4 @@ void shellebrate_loop(void) {
 
 }
 
-int main(int argc, char** argv) {
-
-    // Call the loop function to get this party started!
-    shellebrate_loop();
-
-    // Leave that party like a boss
-    return EXIT_SUCCESS;
-
-}
+/* ========= LE FIN ========= */
